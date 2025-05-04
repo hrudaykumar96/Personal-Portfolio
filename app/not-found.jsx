39bb9a04579c34156cc0317a3d0683eab35f1d08
Hruday Kumar <130309908+hrudaykumar96@users.noreply.github.com>
2025-05-04 23:29:09 +0530

    Delete app directory

--- app/not-found.jsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation"
-
-const PageNotFound = () => {
-    redirect('/')
-};
-
-export default PageNotFound
--- app/utils/cloudinary/deleteCloudinary.js
+++
@@ -1,8 +0,0 @@
-import cloudinary from "./cloudinary";
-
-const deleteUploadFile = async(publicId, resourceType)=>{
-    const type = resourceType === 'raw' ? 'raw' : 'image';
-    await cloudinary.uploader.destroy(publicId, { resource_type: type });
-};
-
-export default deleteUploadFile;
