2dca9eff4a92e22d94429a10a92d5f1b61227058
Hruday Kumar <130309908+hrudaykumar96@users.noreply.github.com>
2025-05-18 22:37:50 +0530

    Add files via upload

--- app/api/gettoggletheme/route.js
+++ app/api/gettoggletheme/route.js
@@ -2,18 +2,32 @@ import DbConnection from "@/app/utils/config/DbConnection";
 import Effects from "@/app/utils/models/Effects";
 import { NextResponse } from "next/server";
 
+export const GET = async () => {
+  try {
+    await DbConnection();
 
-export const GET = async()=>{
-    try {
-        await DbConnection();
-        const effects = await Effects.findOne();
-        const hour = new Date().getHours();
-        const isDay = hour >= 6 && hour < 18;
-        const mode = isDay ? 'light' : 'dark';
-        effects.mode = mode;
-        await effects.save();
-        return NextResponse.json({ success: effects.mode });
-    } catch (error) {
-        return NextResponse.json({ error: 'Internal server error' });
+    const effects = await Effects.findOne();
+
+    const hour = parseInt(
+      new Date().toLocaleString("en-US", {
+        timeZone: "Asia/Kolkata",
+        hour: "numeric",
+        hour12: false,
+      })
+    );
+
+    const isDay = hour >= 6 && hour < 18;
+    const mode = isDay ? "light" : "dark";
+
+    if (effects) {
+      effects.mode = mode;
+      await effects.save();
+    } else {
+      await Effects.create({ mode });
     }
+
+    return NextResponse.json({ success: mode });
+  } catch (error) {
+    return NextResponse.json({ error: "Internal server error" });
+  }
 };
